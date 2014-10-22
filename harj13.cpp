6cfaffd3e56bc89716ba48e9d63524a8f933a663
Markus Malm <markus.malm@gmail.com>
2014-10-22 12:37:21 +0300

    harjoitus 13
    a) v1.0

---
+++ harj13.cpp
@@ -0,0 +1,42 @@
+﻿/******
+*ohjelman nimi: harjoitus 13
+*tekija Markus Malm
+*Lyhyt kuvaus:
+Tee ohjelma, joka kysyy etunimesi ja sukunimesi. Ohjelma
+yhdist�� nimet yhdeksi merkkijonoksi ja tulostaa ne
+lopuksi nayt�lle.
+
+
+a) Kayta cstring-kirjastoa (C:n merkkitaulukot)
+tehd�ksesi merkkijonojen yhdist�misen
+b) Kayta string-kirjastoa (C++:n merkkijonot)
+tehd�ksesi merkkijonojen yhdist�misen  TEE, JOS ON AIKAA
+
+Esimerkki
+Anna etunimi: Aku
+Anna sukunimi: Ankka
+Nimesi oli: Aku Ankka
+
+*versio 1.0
+*pvm: 22.10.2014
+*/
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+	char etunimi[51];
+	char sukunimi[51];
+	char kokonimi[51];
+
+	cout << "Anna etunimi\n";
+	cin >> etunimi;
+	cout << "Anna sukunimi\n";
+	cin >> sukunimi;
+
+	strcpy_s(kokonimi, etunimi);
+	strcat_s(kokonimi, " ");
+	strcat_s(kokonimi, sukunimi);
+	cout <<"Nimesi oli: "<< kokonimi;
+
+}
